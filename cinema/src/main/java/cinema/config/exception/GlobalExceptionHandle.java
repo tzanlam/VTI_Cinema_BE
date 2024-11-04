4e3cdbdba5981b5fe6f57117fdb9733a32c09329
tzanlam <tzanlam@gmail.com>
2024-11-05 04:45:18 +0700

    code exception valid

---
+++ cinema/src/main/java/cinema/config/exception/GlobalExceptionHandle.java
@@ -0,0 +1,30 @@
+package cinema.config.exception;
+
+import cinema.modal.response.ErrorDetail;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class GlobalExceptionHandle {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> MethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        String message = ex.getBindingResult().getAllErrors().stream()
+                .map(error -> ((FieldError) error).getField()+": "+error.getDefaultMessage())
+                .findFirst().orElse("error validation");
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> BindException(BindException ex, WebRequest request) {
+        ErrorDetail error = new ErrorDetail(new Date(), ex.getMessage(), request.getDescription(false));
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+}
--- cinema/src/main/java/cinema/modal/response/ErrorDetail.java
+++ cinema/src/main/java/cinema/modal/response/ErrorDetail.java
@@ -1,12 +1,14 @@
 package cinema.modal.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.util.Date;
 
 @Data
+@AllArgsConstructor
 public class ErrorDetail {
-    private LocalDate timestamp;
+    private Date timestamp;
     private String message;
     private String detail;
 }
