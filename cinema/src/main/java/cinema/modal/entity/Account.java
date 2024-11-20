01308c1a47cb07da9cc0419288dae226a7a06b96
tzanlam <tzanlam@gmail.com>
2024-11-20 23:30:12 +0700

    code

--- cinema/src/main/java/cinema/modal/entity/Account.java
+++ cinema/src/main/java/cinema/modal/entity/Account.java
@@ -5,11 +5,13 @@ import cinema.modal.entity.constant.StatusAccount;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Data
 @Entity
+@NoArgsConstructor
 @Table(name = "account")
 public class Account extends Base{
     @Id
