8a21ca12cce9deb8ef1c0a32751391277db5452b
tzanlam <tzanlam@gmail.com>
2024-12-06 08:23:13 +0700

    Update scheduled

--- cinema/src/main/java/cinema/util/TaskScheduled.java
+++ cinema/src/main/java/cinema/util/TaskScheduled.java
@@ -1,10 +1,13 @@
 package cinema.util;
 
 import cinema.modal.entity.Booking;
+import cinema.modal.entity.Seat;
 import cinema.modal.entity.Ticket;
 import cinema.modal.entity.constant.StatusBooking;
+import cinema.modal.entity.constant.StatusSeat;
 import cinema.modal.entity.constant.StatusTicket;
 import cinema.repository.BookingRepository;
+import cinema.repository.SeatRepository;
 import cinema.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -21,21 +24,35 @@ public class TaskScheduled {
     @Autowired
     private TicketRepository ticketRepository;
 
+    @Autowired
+    private SeatRepository seatRepository;
+
     @Scheduled(fixedRate = 60000) // Chạy mỗi phút
     public void cancelExpiredBookings() {
         List<Booking> expiredBookings = bookingRepository.findByExpiryTimeBeforeAndStatus(
                 LocalDateTime.now(), StatusBooking.PENDING
         );
         for (Booking booking : expiredBookings) {
-            booking.setStatus(StatusBooking.CANCELLED);
-            bookingRepository.save(booking);
+            try {
+                booking.setStatus(StatusBooking.CANCELLED);
+                bookingRepository.save(booking);
+                cancelTicketsAndUpdateSeats(booking);
 
-            // Hủy các vé liên quan
-            for (Ticket ticket : booking.getTickets()) {
-                ticket.setStatus(StatusTicket.CANCELLED);
-                ticketRepository.save(ticket);
+            } catch (Exception e) {
+                System.err.println("Error cancelling booking with ID: " + booking.getId() + " - " + e.getMessage());
             }
         }
     }
 
+    private void cancelTicketsAndUpdateSeats(Booking booking) {
+        for (Ticket ticket : booking.getTickets()) {
+            ticket.setStatus(StatusTicket.CANCELLED);
+            ticketRepository.save(ticket);
+            Seat seat = ticket.getSeat();
+            if (seat != null) {
+                seat.setStatus(StatusSeat.AVAILABLE);
+                seatRepository.save(seat);
+            }
+        }
+    }
 }
